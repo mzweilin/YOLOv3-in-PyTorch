9b9d30b8974912d3e932972ca68682b30bcf7a28
Weilin Xu <weilin.xu@intel.com>
2023-04-11 10:04:08 -0700

    Include yolov3.datasets in the package.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import setuptools
 
 setuptools.setup(
-    name='yolov3',
-    version='0.0.1',
-    description='YOLOv3 in PyTorch with training and inference module implemented.',
-    url='https://github.com/mzweilin/YOLOv3-in-PyTorch',
-    packages=['yolov3'],
+    name="yolov3",
+    version="0.0.1",
+    description="YOLOv3 in PyTorch with training and inference module implemented.",
+    url="https://github.com/mzweilin/YOLOv3-in-PyTorch",
+    packages=["yolov3", "yolov3/datasets"],
 )
