9677788ceeb6979b1c5ff67b7fdfeadee6f3746c
Weilin Xu <weilin.xu@intel.com>
2023-04-19 12:45:05 -0700

    Add dependency of the package.

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 import setuptools
+import os
+
+# Load dependency. Credit goes to https://stackoverflow.com/a/53069528
+install_requires = []
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirement_path = lib_folder + "/requirements.txt"
+if os.path.isfile(requirement_path):
+    with open(requirement_path) as f:
+        install_requires = f.read().splitlines()
 
 setuptools.setup(
     name="yolov3",
+    install_requires=install_requires,
     version="0.0.1",
     description="YOLOv3 in PyTorch with training and inference module implemented.",
     url="https://github.com/mzweilin/YOLOv3-in-PyTorch",
